eac0266d7e072eacf7976ed8d003e1552324431c
Tatiana-Juin <username@users.noreply.github.com>
2026-01-09 12:24:35 +0100

    j'ai commencer a implementer la fonction qui recupere la valeur du regex et j'ai commencer les verification sur celui ci

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,15 +19,45 @@ let btnCopier = document.querySelector(".btn-copier");
 
 // function pour les différents toogle 
 const funcToggle = (btn,content)=>{
+    // pour ajouter la class hiden au element 
     const isHiden = content.classList.toggle("hiden")
+    // si la class isHiden existe alors on change le logo 
     btn.classList.toggle("bi-arrow-down-short",isHiden)
+    // si la class isHiden = false alors on change de logo 
     btn.classList.toggle("bi-arrow-up-short",!isHiden)
 }
+
 // le toggle de bibliotheque de regex
 toggleBiblio.addEventListener("click", () => {
     funcToggle(toggleBiblio, hidenBiblio); 
 });
+
 // toggle pour afficher les pourcentages 
 toggleScore.addEventListener("click",()=>{
     funcToggle(toggleScore, pourcentage); 
 })
+
+
+// Fonction pour verifier l'expression 
+btnLancer.addEventListener("click",(e) =>{
+    e.preventDefault()
+    let erreur = document.querySelector(".erreur")
+    // pour recuperer la valeur du texte 
+   let regexValue = inputRegex.value.trim();
+   if(regexValue==""){
+     erreur.textContent="Erreur tu dois saisir un regex "
+     erreur.style.color="red";
+   }
+   else{
+    erreur.textContent="";
+    // appelle de la function pour faire les test
+    funcTest(regexValue)
+   }
+   
+
+})
+
+// function pour les test 
+function funcTest(regexValue){
+    console.log(regexValue)
+}
